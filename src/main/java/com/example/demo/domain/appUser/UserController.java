148d2b7e3a36db47c577f7db7a33d01e372d0826
Akbar-Noser <Oliver.Pettersson@noseryoung.ch>
2021-11-11 16:28:40 +0100

    GEN: added default data for list entries

--- src/main/java/com/example/demo/domain/appUser/UserController.java
+++ src/main/java/com/example/demo/domain/appUser/UserController.java
@@ -2,6 +2,7 @@ package com.example.demo.domain.appUser;
 
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -9,10 +10,15 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Collection;
 
 @RestController @RequestMapping("/api")
-@RequiredArgsConstructor
+
 public class UserController {
 //    ADD YOUR ENDPOINT MAPPINGS HERE
 private UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping("/")
     public ResponseEntity<String> HomeTest(){
