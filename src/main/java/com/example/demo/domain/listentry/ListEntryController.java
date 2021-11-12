a48127cd7ae917243330f68557a328f73909f4d6
Akbar-Noser <Oliver.Pettersson@noseryoung.ch>
2021-11-12 13:36:53 +0100

    WEB/SEC: decodeCredentials methode was not added by mistake

--- src/main/java/com/example/demo/domain/listentry/ListEntryController.java
+++ src/main/java/com/example/demo/domain/listentry/ListEntryController.java
@@ -71,5 +71,13 @@ public class ListEntryController {
         }
 
     }
+
+    private String[] decodeCredentials(String authorizationHeader) {
+        String base64Credentials = authorizationHeader.substring("Basic".length()).trim();
+        byte[] credDecoded = Base64.getDecoder().decode(base64Credentials);
+        String credentials = new String(credDecoded, StandardCharsets.UTF_8);
+        // format is "username:password"
+        return credentials.split(":", 2);
+    }
 }
 
